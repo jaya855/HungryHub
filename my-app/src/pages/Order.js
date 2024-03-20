07a1f08ff2f63d6cf4be53ca81151317cf957cf3
jaya855 <srivastavajaya175@gmail.com>
2024-03-21 00:37:12 +0530

    samapti

--- my-app/src/pages/Order.js
+++ my-app/src/pages/Order.js
@@ -6,8 +6,7 @@ import Footer from '../components/Footer';
 const Order = () => {
     
     const [myorder, setMyorder] = useState([]);
-    console.log("heeloo")
-    console.log(myorder)
+
     const handleMYorder = async () => {
         try {
             const myuser = localStorage.getItem("email");
@@ -27,24 +26,38 @@ const Order = () => {
       handleMYorder();
     }, []);
 
+    // Function to convert date to a better understandable format
+    const formatDate = (dateString) => {
+        const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
+        return new Date(dateString).toLocaleDateString(undefined, options);
+    };
+
   return (
-    <div>
-      <Navbar/>
-      <div>
+    <div style={{ backgroundColor: "#000", color: "#fff" }}>
+      <Navbar />
+      <div style={{ backgroundColor: "#000", color: "#fff", paddingTop: "20px" }}> {/* Added paddingTop */}
         {
-          myorder.map((ele, id) => (
-            <div key={id}>
-                <div>{ele.userDate}</div>
-              
-              {ele.userItem && ele.userItem.map((item, itemId) => (
-                <div key={itemId}>
-                  <div>{item.fdName}</div>
-                  <div>{item.otp}</div>
-                  <div>{item.price}</div>
-                  <div>{item.qnty}</div>
-                </div>
-              ))}
-            </div>
+          myorder.map((element) => (
+            element.map((ele, id)=>(
+              <div key={id}>
+                  <div style={{"text":"green" , "backgroundColor":"#944E63","fontWeight":"bold"}}>Time { }: {formatDate(ele.userDate)}</div>
+                  <hr/>
+                  <br/>
+                
+                  {Object.entries(ele.userItem).map(([itemName, item], itemId) => (
+                      <div key={itemId} style={{'fontWeight':"bold"}} >
+                          <div >FoodName {" : "} {item.fdName}</div>
+                          <div>Option {" : "} {item.opt}</div>
+                          <div>Price {" : "} {item.price}</div>
+                          <div>Quantity {" : "} {item.qnty}</div>
+                          <hr/>
+                      </div>
+                      
+                  ))}
+                  <hr/>
+              </div>
+
+            ))
           ))
         }
       </div>
